b8da2fe2e34fb202d260f3800538189456b35aa2
Tạ Thúc Trung Kiên <tathuctrungkien2003@gmail.com>
2025-03-21 15:06:04 +0700

    Add 404 page

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -35,7 +35,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
         <div className="container flex flex-wrap justify-between items-center">
           {/* logo */}
           <Link href="/" className="flex items-center">
-            <Logo loading="eager" priority="high" className="invert dark:invert-0" />
+            <Logo loading="eager" priority="high" className="invert-0" />
           </Link>
           {/* nav bar */}
           <HeaderNav data={data} />
--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -5,14 +5,20 @@ import { Button } from '@/components/ui/button'
 
 export default function NotFound() {
   return (
-    <div className="container py-28">
-      <div className="prose max-w-none">
-        <h1 style={{ marginBottom: 0 }}>404</h1>
-        <p className="mb-4">This page could not be found.</p>
+    <div className="bg-white dark:bg-gray-900">
+      <div className="container py-8 lg:py-16 h-full">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+            404
+          </h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+            This page could not be found.
+          </p>
+          <Button asChild variant="default">
+            <Link href="/">Go home</Link>
+          </Button>
+        </div>
       </div>
-      <Button asChild variant="default">
-        <Link href="/">Go home</Link>
-      </Button>
     </div>
   )
 }
