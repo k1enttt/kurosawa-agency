8f9f332b9357a96e3cde1efa1c787d089222103e
Tạ Thúc Trung Kiên <tathuctrungkien2003@gmail.com>
2025-03-26 20:18:48 +0700

    Add news page and remove seminars, newsletter and blogs page

---
+++ src/app/(frontend)/news/page/[pageNumber]/page.tsx
@@ -0,0 +1,95 @@
+import type { Metadata } from 'next/types'
+
+import { CollectionArchive } from '@/components/CollectionArchive'
+import { PageRange } from '@/components/PageRange'
+import { Pagination } from '@/components/Pagination'
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+import React from 'react'
+import PageClient from './page.client'
+import { notFound } from 'next/navigation'
+
+export const revalidate = 600
+
+type Args = {
+  params: Promise<{
+    pageNumber: string
+  }>
+}
+
+export default async function Page({ params: paramsPromise }: Args) {
+  const { pageNumber } = await paramsPromise
+  const payload = await getPayload({ config: configPromise })
+
+  const sanitizedPageNumber = Number(pageNumber)
+
+  if (!Number.isInteger(sanitizedPageNumber)) notFound()
+
+  const news = await payload.find({
+    collection: 'posts',
+    depth: 1,
+    limit: 12,
+    page: sanitizedPageNumber,
+    overrideAccess: false,
+    where: {
+      'categories.slug': {
+        equals: 'blog',
+      },
+    },
+  })
+
+  return (
+    <div className="pt-24 pb-24">
+      <PageClient />
+      <div className="container mb-16">
+        <div className="prose dark:prose-invert max-w-none">
+          <h1>News</h1>
+        </div>
+      </div>
+
+      <div className="container mb-8">
+        <PageRange
+          collection="posts"
+          currentPage={news.page}
+          limit={12}
+          totalDocs={news.totalDocs}
+        />
+      </div>
+
+      <div className="container">
+        <CollectionArchive posts={news.docs} />
+      </div>
+
+      <div className="container">
+        {news?.page && news?.totalPages > 1 && (
+          <Pagination slug="news" page={news.page} totalPages={news.totalPages} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
+  const { pageNumber } = await paramsPromise
+  return {
+    title: `Kurosawa News Page ${pageNumber || ''}`,
+  }
+}
+
+export async function generateStaticParams() {
+  const payload = await getPayload({ config: configPromise })
+  const { totalDocs } = await payload.count({
+    collection: 'posts',
+    overrideAccess: false,
+  })
+
+  const totalPages = Math.ceil(totalDocs / 10)
+
+  const pages: { pageNumber: string }[] = []
+
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push({ pageNumber: String(i) })
+  }
+
+  return pages
+}
