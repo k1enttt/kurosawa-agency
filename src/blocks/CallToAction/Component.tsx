dde772e80c8c602cd3c344c0cf65935adcf01ff5
Tạ Thúc Trung Kiên <tathuctrungkien2003@gmail.com>
2025-05-07 21:27:13 +0700

    Update CTA block

--- src/blocks/CallToAction/Component.tsx
+++ src/blocks/CallToAction/Component.tsx
@@ -7,15 +7,31 @@ import { CMSLink } from '@/components/Link'
 
 export const CallToActionBlock: React.FC<CTABlockProps> = ({ links, richText }) => {
   return (
-    <div className="container">
-      <div className="bg-card rounded border-border border p-4 flex flex-col gap-8 md:flex-row md:justify-between md:items-center">
-        <div className="max-w-[48rem] flex items-center">
-          {richText && <RichText className="mb-0" data={richText} enableGutter={false} />}
-        </div>
-        <div className="flex flex-col gap-8">
-          {(links || []).map(({ link }, i) => {
-            return <CMSLink key={i} size="lg" {...link} />
-          })}
+    <div className="bg-primary">
+      <div className="container">
+        <div className="rounded py-10 flex flex-col gap-8 md:flex-row md:justify-between md:items-center">
+          <div className="max-w-[48rem] flex items-center">
+            {richText && (
+              <RichText
+                className="mb-0 text-lg text-primary-foreground"
+                data={richText}
+                enableProse={false}
+                enableGutter={false}
+              />
+            )}
+          </div>
+          <div className="flex flex-col gap-8">
+            {(links || []).map(({ link }, i) => {
+              return (
+                <CMSLink
+                  key={i}
+                  size="lg"
+                  {...link}
+                  className="font-bold text-foreground bg-primary-foreground hover:bg-primary-foreground"
+                />
+              )
+            })}
+          </div>
         </div>
       </div>
     </div>
